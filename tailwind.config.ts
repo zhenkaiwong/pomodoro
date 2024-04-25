ec8da6bf03f0718ddfb570e514fa700063d391db
zhenkaiwong1996 <zhenkaiwong.work@gmail.com>
2024-04-25 21:51:30 +0800

    Add app theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,18 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        focus: "#c66",
+        rest: "#6cc",
+        neutral: "#eff9f9",
+        buttonShadow: "#eb",
+      },
+      boxShadow: {
+        button: "rgb(235, 235, 235) 0px 12px 0px;",
       },
     },
   },
