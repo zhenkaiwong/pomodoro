34e116ce553b22e989c4a8eb3dcb178bfe9369e1
zhenkaiwong1996 <zhenkaiwong.work@gmail.com>
2024-04-25 22:07:04 +0800

    Add feature to read timer duration from environment variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Clock from "@/lib/components/Clock";
 
 export default function Home() {
-  return <Clock />;
+  const focusDuration = Number(process.env.FOCUS_DURATION || 0.25);
+  const restDuration = Number(process.env.REST_DURATION || 0.25);
+
+  return <Clock focusDuration={focusDuration} restDuration={restDuration} />;
 }
